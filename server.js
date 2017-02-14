6917b14d9e8bdd997b2604257952fb7d92a8c2fd
knc9792 <nagchaitu784@gmail.com>
2017-02-14 10:11:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,56 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var articleOne={
+    title:"Article-One |Chaitanya",
+    date:" Feb 14th 2017",
+    heading:"ArticleOne",
+    content:` <p>This is article one </p>
+                <p>This is article one </p>
+                <p>This is article one </p>
+                <p>This is article one </p>
+                <p>This is article one </p>`
+                
+};
+function createTemplate(data){
+    
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    var date = data.date;
+var htmlTemplate=`<html>
+    <head>
+        <title> ${title}</title>
+        <meta name="viewport" content="width=device-width initial-scale=1">
+    <style>
+        .container{
+            max-width:800px;
+            
+        }
+    </style>
+    </head>
+    
+    <body>
+        <div class="container">
+            <div>
+            <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>${heading}</h3>
+            <div>
+               ${date}
+            </div>
+            <div>
+              ${content}
+            </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+} 
+
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -17,7 +67,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send(createTemplate(articleOne));
 });
 
 
