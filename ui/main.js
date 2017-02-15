134f30a03d2530cd9a4e073606cd53299eaac0a9
knc9792 <nagchaitu784@gmail.com>
2017-02-15 10:06:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
-elemenet.innerHTML="Hello world";
+element.innerHTML="Hello world";
 var image=document.getElementById("imageid");
 image.onclick=function(){
     image.style.marginleft="100px";
