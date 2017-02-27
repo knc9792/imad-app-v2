9d999ff968861dabecba68d9ccb4017e34561c19
knc9792 <nagchaitu784@gmail.com>
2017-02-27 17:16:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
   database: 'knc9792',
   host:'db.imad.hasura-app.io',
   port:'5432',
-  password:process.env.DB_PASSWORD
+  password:'db-knc9792-90907'
   
 };
 var pool = new Pool(config);
