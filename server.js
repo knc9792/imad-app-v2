4de933f4d5753047228b1bf33060cc2a11b90f56
knc9792 <nagchaitu784@gmail.com>
2017-02-27 17:35:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,9 +109,25 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/:articleName', function (req, res) {
-    var articleName= req.params.articleName;
- res.send(createTemplate(articleName));
+app.get('/article/:articleName', function (req, res) {
+    
+ pool.query("SELECT * FROM articles WHERE title="+req.params.articleName, function(err,result){
+     if(err){
+         res.status(500).send(err.toString());
+     }
+     else{
+         if(result.rows.length===0){
+             res.status(404).send('Requested article not found');
+         }
+         else{
+      var articleData = result.rows[0];
+      res.send(createTemplate(articleData));
+     }
+     }
+ });
+ 
+ 
+ res.send(createTemplate(articleData));
 });
 
 
