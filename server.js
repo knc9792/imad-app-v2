3c0d0ea0bab5692ecab74a0788ca526fd6e2dc05
knc9792 <nagchaitu784@gmail.com>
2017-02-27 17:11:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
-
+var config = {
+  user: 'knc9792',
+  database: 'knc9792',
+  host:'db.imad.hasura-app.io',
+  port:'5432',
+  password:process.env.DB_PASSWORD
+  
+};
+var pool = new Pool(config);
 var articles={
     
 articleOne:{
@@ -74,6 +83,17 @@ return htmlTemplate;
 
 var app = express();
 app.use(morgan('combined'));
+
+
+app.get('/test-db',function(req,res){
+   pool.query('Select * from dbtest', function(err,result) {
+    if (err){
+        res.status(500).send(err.toString());
+    } else{
+        res.send(JSON.stringfy(result));
+    }
+});
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
